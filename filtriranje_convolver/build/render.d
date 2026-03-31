fe6c98069988d5fcb4157163ffb7be7b71dc3c42
Marija-Ratkovic <rm@etf.bg.ac.rs>
2026-03-31 14:41:14 +0000

    dodato filtriranje u vremenskom domenu

---
+++ filtriranje_convolver/build/render.d
@@ -0,0 +1,27 @@
+/root/Bela/projects/filtriranje_convolver/build/render.o: \
+  /root/Bela/projects/filtriranje_convolver/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  /usr/xenomai/include/cobalt/time.h include/Utilities.h \
+  libraries/Convolver/Convolver.h libraries/ne10/NE10.h \
+  libraries/AudioFile/AudioFile.h libraries/sndfile/sndfile.h
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+/usr/xenomai/include/cobalt/time.h:
+
+include/Utilities.h:
+
+libraries/Convolver/Convolver.h:
+
+libraries/ne10/NE10.h:
+
+libraries/AudioFile/AudioFile.h:
+
+libraries/sndfile/sndfile.h:
---
+++ filtriranje_convolver/render.cpp
@@ -0,0 +1,68 @@
+#include <Bela.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <libraries/Convolver/Convolver.h>
+#include <libraries/AudioFile/AudioFile.h>
+
+
+#define NUM_OF_SPEAKERS 12
+#define W_LEN 1024
+
+std::vector<float> data;
+Convolver convolver;
+
+const unsigned int gInputChannel = 1;
+
+
+bool setup(BelaContext *context, void *userData)
+{
+	std::ifstream file("w_eq_real_LMS.csv");
+    std::string line;
+    std::vector<std::vector<float>> w;
+    
+    while (std::getline(file, line)) {
+        std::istringstream iss(line);
+        std::string value;
+        std::vector<float> row;
+
+        while (std::getline(iss, value, ',')) {
+            row.push_back(std::stod(value));
+        }
+        w.push_back(row);
+    }
+    convolver.setup (w,  context->audioFrames);
+    
+     //rt_printf("%d\n",context->audioFrames);
+    
+	return true;
+}
+
+void render(BelaContext *context, void *userData)
+{
+	float inBuf[context->audioFrames];
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		float in = 0;
+		in = audioRead(context, n, gInputChannel);
+		inBuf[n] = in;
+	}
+	
+	//rt_printf("%f ",inBuf[1000]);
+
+	float outBuf[context->audioFrames];
+	convolver.process(outBuf, inBuf, context->audioFrames);
+
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		for(unsigned int channel = 0; channel < NUM_OF_SPEAKERS; ++channel)
+			audioWrite(context, n, channel, 2*outBuf[n]);
+		//	audioWrite(context, n, channel, 0.8*inBuf[n]);
+	}
+	
+}
+
+void cleanup(BelaContext *context, void *userData)
+{
+
+}
